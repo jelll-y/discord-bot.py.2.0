935b0a74f2f1142798169ddec146b64f323d8a55
Harris Baker <harrisbaker02@outlook.com>
2023-04-25 09:36:07 +1000

    Added AI Image command

--- cogs/ai.py
+++ cogs/ai.py
@@ -12,18 +12,25 @@ class ai(commands.Cog):
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('AI command is loaded')
+        print('AI commands are loaded')
 
     @commands.command()
     async def ai(self, ctx: commands.Context, *, prompt: str):
-        print(prompt)
         response = openai.Completion.create(
             engine="text-davinci-003",
             prompt=f"{prompt}",
             max_tokens=100,
             temperature=0.5)
-        print(response)
         await ctx.reply(response.choices[0].text)
+
+    @commands.command()
+    async def image(self, ctx: commands.Context, *, prompt: str):
+        response = openai.Image.create(
+            prompt=f"{prompt}",
+            n=1,
+            size="512x512")
+        print(response)
+        await ctx.reply(response.data[0].url)
 
 
 async def setup(bot):
