34ed6e443d815293d17da814a91d9fa781ac2ef5
Harris Baker <harrisbaker02@outlook.com>
2024-07-09 17:08:34 +1000

    fixed image cog to not include the ai command

--- cogs/image.py
+++ cogs/image.py
@@ -1,96 +1,41 @@
 import discord
 from discord.ext import commands
-
-import pathlib
-import textwrap
-
-import google.generativeai as genai
-from IPython.display import display
-from IPython.display import Markdown
-
 import config
+import os
 
-import requests
-from PIL import Image
-import openai
-
-openai.api_key = config.CHATGPT_API
-api_key = config.GOOGLE_API
-genai.configure(api_key=api_key)
-
-# Set up the model
-generation_config = {
-    "temperature": 0.9,
-    "top_p": 1,
-    "top_k": 1,
-    "max_output_tokens": 2048,
-}
-
-safety_settings = [
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE"
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE"
-    },
-]
+import textwrap
+from openai import OpenAI
+os.environ["OPENAI_API_KEY"] = config.CHATGPT_API
+client = OpenAI()
 
 
-def to_markdown(text):
-    text = text.replace('•', '  *')
-    return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
-
-def generate(text):
-    res = openai.Image.create(
-        prompt=text,
-        n=1,
-        size="512x512",
+def response(prompt):
+    ai_image = client.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1
     )
-    return res["data"][0]["url"]
+    print(ai_image)
+    return ai_image.data[0].url
 
 
-class ai(commands.Cog):
+class image(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print('AI commands are loaded')
+        print('Image commands are loaded')
 
-    @commands.hybrid_command(
-        name="ai",
-        description="Uses Google Gemini AI to provide responses.")
-    async def ai(self, ctx: commands.Context, *, prompt: str):
-        # noinspection PyTypeChecker
-        model = genai.GenerativeModel(model_name="gemini-1.0-pro",
-                                      generation_config=generation_config,
-                                      safety_settings=safety_settings)
-        response = model.generate_content(f'{prompt}')
-        await ctx.reply(response.text)
-
-    @commands.hybrid_command(
+    @commands.command(
         name='image',
         description="Uses ChatGPT AI for image generation.")
     async def image(self, ctx: commands.Context, *, prompt: str):
-        text = prompt
-        print(text)
-        url1 = generate(text)
-        print(url1)
-        response = requests.get(url1)
-        print(response)
-        await ctx.reply(response.raw)
+        img_obj_string = str(response(prompt))
+        print(textwrap.fill(img_obj_string, width=140))
 
 
 async def setup(bot):
-    await bot.add_cog(ai(bot))
+    await bot.add_cog(image(bot))
