75ab623c8363a0ad8fde79ab67e43bc393b4edda
Harris Baker <harrisbaker02@outlook.com>
2024-07-20 12:08:13 +1000

    Fixed a small bug with printing the random options

--- cogs/random.py
+++ cogs/random.py
@@ -20,7 +20,28 @@ class Random(commands.Cog):
         if len(option) == 0:
             await ctx.send(f"Provide a few options {r.choice(self.bot.names)}.")
         else:
-            options = ' '.join(option)
+            print(option)
+            options = ''.join(option)
+            choice_list = options.split(', ')
+            choice = r.choice(choice_list)
+            if choice == 'kfc':
+                await ctx.send('https://tenor.com/view/colonel-sanders-shirtless-abs-dancing-sparkles-gif-17679994')
+            elif choice == 'maccas':
+                await ctx.send('https://tenor.com/view/good-yes-mcdonalds-mcdo-thumbs-up-gif-16886848')
+            else:
+                await ctx.send(f'{choice}')
+                
+    
+    @commands.command(
+        name="r",
+        description="Will choose a random option from your specified list."
+    )
+    async def r(self, ctx: commands.Context, *, option: str):
+        if len(option) == 0:
+            await ctx.send(f"Provide a few options {r.choice(self.bot.names)}.")
+        else:
+            print(option)
+            options = ''.join(option)
             choice_list = options.split(', ')
             choice = r.choice(choice_list)
             if choice == 'kfc':
